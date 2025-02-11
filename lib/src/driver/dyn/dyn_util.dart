e90f45b50d1c9c049466ef8e0fdc4c30986aaec2
zhiming_liang_a <479170670@qq.com>
2025-02-11 12:24:49 +0800

    implement dll call

--- lib/src/driver/dyn/dyn_util.dart
+++ lib/src/driver/dyn/dyn_util.dart
@@ -1,7 +1,70 @@
-import 'dart:ffi';
 import 'model.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart' as path;
 
-Map<String, dynamic> runDynFunction(FunctionInfo functionInfo) {
-  ///TODO add implementation
-  return {};
+Future<Map<String, dynamic>> runDynFunction(FunctionInfo functionInfo) async {
+  try {
+    final executablePath = await _getDynBridgeExecutablePath();
+
+    final dynfilePath = functionInfo.dynFile.path;
+    final funcName = functionInfo.name;
+    final params = jsonEncode([
+      for (var param in functionInfo.parameterInfoList.where((p) => p.isIn))
+        {'type': param.cDataType, 'is_pointer': param.isPointer, 'value': param.value}
+    ]);
+    final returnInfo = functionInfo.parameterInfoList.where((p) => !p.isIn).firstOrNull;
+    final returnType = jsonEncode({
+      'type': returnInfo?.cDataType ?? "int",
+      'is_pointer': returnInfo?.isPointer ?? true
+    });
+
+    // print("python executablePath: $executablePath runDynFunction: ${functionInfo.name}, params: $params, returnType: $returnType");
+
+    final process = await Process.start(executablePath, [
+      '--lib_path=$dynfilePath',
+      '--func_name=$funcName',
+      '--params=$params',
+      '--return_type=$returnType',
+    ]);
+
+    final output = await process.stdout.transform(utf8.decoder).join();
+    final error = await process.stderr.transform(utf8.decoder).join();
+    final exitCode = await process.exitCode;
+    
+    if (exitCode != 0) {
+      throw Exception('Call dynamic library bridge failed: $error');
+    }
+  
+    return jsonDecode(output);
+  } catch (e) {
+    throw Exception('Call dynamic library function failed: $e');
+  }
+}
+
+Future<String> _getDynBridgeExecutablePath() async {
+  String executableName;
+  
+  if (Platform.isWindows) {
+    executableName = 'dyn_bridge.exe';
+  } else if (Platform.isMacOS) {
+    var result = await Process.run('uname', ['-m']);
+    if (result.stdout.toString().trim() == 'arm64') {
+      executableName = 'dyn_bridge_arm64'; // macOS ARM
+    } else {
+      executableName = 'dyn_bridge_x86_64'; // macOS Intel
+    }
+  } else {
+    executableName = 'dyn_bridge'; // Linux
+  }
+  
+  final projectDir = Directory.current.path;
+  final executablePath = path.join(projectDir, "libs", "dyn_bridge", Platform.operatingSystem.toLowerCase(), executableName);
+
+  if (!Platform.isWindows) {
+    await Process.run('chmod', ['+x', executablePath]);
+  }
+
+  return executablePath;
 }
